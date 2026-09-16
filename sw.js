de05afef0d1c9e4b88db9e58dc9bee0b3d12600b
IvyZhang1113 <MZHA464@emory.edu>
2026-09-16 10:59:50 -0400

    Fix iPhone PWA icon cache

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE="family-weather-v4-5-3";
-const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./favicon.ico",
-"./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png","./icons/icon-maskable-512.png"];
+const CACHE="family-weather-v4-5-3-icon-v2";
+const A=["./","./index.html","./styles.css","./app.js","./manifest-v2.webmanifest","./favicon.ico",
+"./icons/icon-180.png","./icons/icon-192-v2.png","./icons/icon-512-v2.png","./icons/icon-maskable-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
